f76bf17a645b14f10b036afeb2c403017a574b6a
Nirmit18 <114016215+Nirmit18@users.noreply.github.com>
2023-03-03 23:04:13 +0530

    Create flexbox.cpp

---
+++ CSS/flexbox.cpp
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+<style>
+  .d{
+
+    display: flex;
+    background-color: #1e90ff;
+    flex-flow: row wrap;
+    justify-content: center;
+  }
+  @media (max-width:500px) {
+    .d{
+      flex-direction: column;
+    }
+    
+  }
+  .d>div{
+    background-color: #f1f1f1;
+    margin: 10px;
+    padding: 20px;
+    font-size: 30px;
+  }
+</style>
+</head>
+<body>
+  <div class="d">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>11</div>
+    <div>22</div>
+    <div>32</div>
+    <div>15</div>
+    <div>27</div>
+    <div>30</div>
+    <div>100</div>
+    <div>225</div>
+    <div>314</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+
+  </div>
+</body>
+</html>
